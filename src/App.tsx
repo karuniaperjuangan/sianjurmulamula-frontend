58ed69add819853c5b32b685ac0be7abd1cb8556
karuniaperjuangan <karuniaperjuangan@mail.ugm.ac.id>
2023-07-11 11:07:48 +0700

    fix navbar, add intro

--- src/App.tsx
+++ src/App.tsx
@@ -3,30 +3,21 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 //import './App.css'
 import Navbar from './components/Navbar'
-
+import BackgroundImage from './assets/BG.jpg'
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
+    <div className='h-screen relative flex justify-center'>
+       <img src={BackgroundImage} className='w-full object-cover'></img>
+       <h1 className='absolute text-2xl sm:text-6xl smm-title my-auto mx-auto top-1/2 font-made-sunflower text-smm-red stroke-white hover:scale-110 transition-all'>Sianjur Mula-Mula</h1>
+    </div>
+   
     <div className='w-screen font-made-sunflower h-[5000px]'>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" className=' hover:scale-150'>
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p className=' text-4xl text-red-700'>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
+
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>   
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -47,28 +47,28 @@ export default function Navbar() {
 
 
   return (
-    <div className={`fixed top-0  inline-block  w-full font-made-sunflower h-16`}>
-      <nav className={` top-0 w-full transition z-10 text-white  ${isSticky ? " bg-smm-red shadow-2xl " : " absolute top-0 bg-transparent"} `}>
+    <div className={`fixed top-0  inline-block  w-full font-made-sunflower h-14`}>
+      <nav className={` w-full transition z-10 text-white  ${isSticky ? " bg-smm-red shadow-2xl " : "bg-transparent"} `}>
         <div className=" mx-auto w-[90%] h-full flex justify-between items-center">
           <Link to="/" className={` transition-all ${isSticky?"":"invisible"}`}>
           <div className=" transition-all inline-flex align-middle items-center hover:scale-110">
             
               <img alt='' src={Logo} className='h-10 transition-all'></img>
             
-            <h1 className=" m-4 text-base lg:text-xl  font-extrabold transition-colors text-white">Sianjur Mula-Mula</h1>
+            <h1 className=" mx-4 text-base lg:text-xl  font-extrabold transition-colors text-white">Sianjur Mula-Mula</h1>
           </div>
           </Link>
 
           <ul className='hidden md:inline-flex justify-around list-none items-center h-full'>           {
               link_list.map((link) => {
                 return (
-                  <Link to={link.link} className={`px-4 transition-colors h-16 flex text-white ${isSticky?"hover:bg-black hover:text-white":" hover:text-black"}`}><li className='my-auto'>{link.name}</li></Link>
+                  <Link to={link.link} className={`px-4 transition-colors h-14 flex text-white ${isSticky?"hover:bg-black hover:text-white":" hover:text-black"}`}><li className='my-auto'>{link.name}</li></Link>
                 )
               })
             }
            
           </ul>
-          <div className='md:hidden hover:scale-110 transition-all' onClick={() => setIsOpened(!isOpened)}>
+          <div className='md:hidden hover:scale-110 transition-all h-14 flex align-middle items-center' onClick={() => setIsOpened(!isOpened)}>
             <svg
               className="fill-current h-3 w-3"
               viewBox="0 0 20 20"
@@ -82,7 +82,7 @@ export default function Navbar() {
         </div>
       </nav >
       {isOpened && (<div className=' md:hidden '>
-        <ul className='transition-all bg-blue-500 shadow-md fixed right-0 list-none text-white z-10'>
+        <ul className='transition-all bg-smm-red shadow-md fixed right-0 list-none text-white z-10'>
         {link_list.map((link) => {
           return (
             <li className=' p-4 px-8'><Link to={link.link} className='transition-colors text-white hover:bg-black'>{link.name}</Link></li>
