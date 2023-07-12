45b93b99fc94cf9691b33e4e55b034b0c0c8ce91
karuniaperjuangan <karuniaperjuangan@mail.ugm.ac.id>
2023-07-12 17:20:37 +0700

    Update 404.tsx

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -5,9 +5,9 @@ import Footer from "../components/Footer";
 export default function NotFound() {
   return (
     <>
-    <div className="h-screen w-screen flex justify-center items-center flex-col bg-smm-black">
-      <h1 className="text-4xl font-made-sunflower text-smm-white my-4">404 | Halaman tidak ditemukan</h1>
-      <Link to="/" className="text-2xl font-made-sunflower text-smm-pink">Kembali ke halaman utama</Link>
+    <div className="h-screen w-screen flex justify-center items-center flex-col p-6 bg-smm-black">
+      <h1 className="text-4xl font-made-sunflower text-smm-white my-4 text-center">404 | Halaman tidak ditemukan</h1>
+      <Link to="/" className="text-2xl font-made-sunflower text-smm-pink text-center">Kembali ke halaman utama</Link>
     </div>
     <Navbar />
     <Footer/>
