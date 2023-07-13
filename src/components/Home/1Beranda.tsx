b5da46c9ad62af1c5cc65ddbebe6802724f6ac22
karuniaperjuangan <karuniaperjuangan@mail.ugm.ac.id>
2023-07-13 08:07:16 +0700

    Revert "Mula-Mula -> Mulamula"
    This reverts commit 512d8b6e91b9e39df45ad6bcb25d419ce8d55f47.

--- src/components/Home/1Beranda.tsx
+++ src/components/Home/1Beranda.tsx
@@ -5,8 +5,8 @@ export default function Beranda() {
         <div className='h-screen relative flex justify-center'>
         <img src={BackgroundImage} className='w-full object-cover brightness-50'></img>
         <div className='absolute top-1/3 md:top-1/2 my-auto mx-auto'>
-        <h1 className='text-3xl sm:text-6xl smm-title top-1/2 font-made-sunflower text-smm-red stroke-white hover:scale-105 transition-all'>Sianjur Mulamula</h1>
-        <p className='font-montserrat max-w-2xl w-4/5 mx-auto py-4 text-center hover:scale-105 transition-all'>Sianjur Mulamula adalah sebuah kecamatan yang berada di Kabupaten Samosir, provinsi Sumatera Utara, Indonesia. Ibu kota kecamatan ini berada di desa Ginolat. Sianjur Mulamula terletak di jalan utama yang menghubungi Doloksanggul dan Sidikalang.</p>
+        <h1 className='text-3xl sm:text-6xl smm-title top-1/2 font-made-sunflower text-smm-red stroke-white hover:scale-105 transition-all'>Sianjur Mula-Mula</h1>
+        <p className='font-montserrat max-w-2xl w-4/5 mx-auto py-4 text-center hover:scale-105 transition-all'>Sianjur Mula-mula adalah sebuah kecamatan yang berada di Kabupaten Samosir, provinsi Sumatera Utara, Indonesia. Ibu kota kecamatan ini berada di desa Ginolat. Sianjur Mulamula terletak di jalan utama yang menghubungi Doloksanggul dan Sidikalang.</p>
         </div>
       </div>
     )
