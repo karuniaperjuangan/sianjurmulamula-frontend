289a9343be2f57a2bb3ae415b9b1f43767f45335
karuniaperjuangan <karuniaperjuangan@mail.ugm.ac.id>
2023-07-15 20:26:31 +0700

    integrate graphql from wordpress to website

--- src/pages/Artikel.tsx
+++ src/pages/Artikel.tsx
@@ -1,21 +1,51 @@
 import { useParams } from "react-router-dom";
 import { placeholderWordpressContent } from "../components/placeholder_html";
 import sanitizeHtml from "sanitize-html";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function Artikel() {
     const { id } = useParams();
+
+    const wp_url = "https://sianjur-mulamula.com/wordpress/graphql"
+    const query = `query getPost {
+        post(id: "${id}", idType: DATABASE_ID) {
+          id
+          title
+          content
+        }
+      }`
+
+      const [post, setPost] = useState(null)
+  
+      useEffect(() => {
+        async function getPosts() {
+          const response = await axios.post(wp_url, { query })
+          setPost(response.data.data.post)
+        }
+        getPosts()
+      }, [])
+
+
     return (
         <div className="min-h-screen w-screen bg-smm-black py-14">
-        <h1 className="smm-title font-made-sunflower py-4">{id}</h1>
-        <div dangerouslySetInnerHTML={{ __html: sanitizeHtml(placeholderWordpressContent,
+        {
+            post && <div>
+                <h1 className="smm-title font-made-sunflower py-4">{post.title}</h1>
+        <div dangerouslySetInnerHTML={{ __html: sanitizeHtml(post.content,
         {
             allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img']),
+            //allow classes
             allowedAttributes: {
                 ...sanitizeHtml.defaults.allowedAttributes,
                 'img': ['src', 'alt']
+            
             }
         })        
-        }} className="px-10 max-w-4xl text-justify mx-auto" />
+        }} className="px-10 max-w-4xl text-justify mx-auto flex items-center flex-col" />
+        </div>   
+        }
+        {!post && <h1>Loading...</h1>}
         </div>
     );
     }
--- src/pages/KatalogArtikel.tsx
+++ src/pages/KatalogArtikel.tsx
@@ -1,22 +1,59 @@
 import { Link } from "react-router-dom";
 import BG from '../assets/BG.jpg'
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const wp_url = "https://sianjur-mulamula.com/wordpress/graphql"
+const query = `query getPostsBudaya {
+  posts(where: {categoryId: 3}) {
+    edges {
+      node {
+        id
+        databaseId
+        title
+        featuredImage {
+          node {
+            id
+            link
+          }
+        }
+      }
+    }
+  }
+}`
+
 export default function KatalogArtikel() {
+
+    const [posts, setPosts] = useState([])
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+      async function getPosts() {
+        const response = await axios.post(wp_url, { query })
+        setPosts(response.data.data.posts.edges)
+        setLoading(false)
+      }
+      getPosts()
+    }, [])
+
+
+
     return (
       <>
       <div className="min-h-screen w-screen flex flex-col py-24 bg-white">
       <h1 className="smm-title font-made-sunflower">Ensiklopedia</h1>
-      <div className="w-4/5 max-w-4xl mx-auto">
+      <div className="w-full p-14 md:p-[10%] mx-auto">
       <h2 className=" text-smm-pink text-4xl font-bold mx-4 py-6">Budaya</h2>
       <div className="md:grid md:grid-cols-4">
         {
-            [1,2,3,4,5,6].map((item, index) => {
+           posts.length >0? posts.map((item, index) => {
                 return(
-                    <Link to={`/artikel/${item}`} className="text-2x mx-4 font-made-sunflower text-smm-pink text-center">
-                        <img src={BG} className="aspect-square w-full"/>
-                        <h2 className=" font-montserrat text-black text-2xl font-bold text-left">Artikel </h2>
+                    <Link to={`/artikel/${item.node.databaseId}`} className="text-2x mx-4 my-4 font-made-sunflower text-smm-pink text-center">
+                        <img src={item.node.featuredImage.node.link} className="aspect-square object-cover rounded-md w-full transition-all hover:scale-105"/>
+                        <h2 className=" py-4 font-montserrat text-black transition-all hover:scale-105 text-2xl font-bold text-left">{item.node.title}</h2>
                     </Link>
                 )
-            })
+            }) : <h1>Loading...</h1>
         }
       </div>  
       </div>  
