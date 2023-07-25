336a2ef3ffd345b2558c97fa57af508a8140de5b
karuniaperjuangan <karuniaperjuangan@mail.ugm.ac.id>
2023-07-25 10:06:53 +0700

    fix fonts not monteserrat

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme')
+const fontFamily = defaultTheme.fontFamily;
+fontFamily['sans'] = [
+  'Montserrat', // <-- Roboto is a default sans font now
+  'sans-serif',
+  'system-ui',
+];
+
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily: fontFamily,
     extend: {
       colors:{
         'smm-pink': '#FD0054',
