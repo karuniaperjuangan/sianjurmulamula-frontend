db9536d5ce1fd692b1e038a5d4d14e6be0d6d105
karuniaperjuangan <karuniaperjuangan@mail.ugm.ac.id>
2023-07-25 10:07:10 +0700

    scroll top when change pages

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,8 +1,14 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 
 export default function Root() {
+    const { pathname } = useLocation()
+    useEffect(() => {
+        window.scrollTo(0, 0)
+    }, [pathname])
     return (
         <>
             {/* all the other elements */}
